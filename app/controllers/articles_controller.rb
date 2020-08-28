556bf86512d23366e0b136bdb32935479403fbf6
jacobrees <jacob.rees97@yahoo.com>
2020-08-28 13:20:35 +0100

    Add edit and update method to each individual article. Add flash that notifies user that article update was successful. set #articles as root

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
     include ArticlesHelper
-    
+
     def index 
         @articles = Article.all
     end
@@ -23,6 +23,18 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
         @article.destroy
         redirect_to articles_path
+    end
 
+    def edit
+        @article = Article.find(params[:id])
+      end
+
+    def update
+      @article = Article.find(params[:id])
+      @article.update(article_params)
+
+      flash.notice = "Article '#{@article.title}' Updated!"
+      
+      redirect_to article_path(@article)
     end
 end
