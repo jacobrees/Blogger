0f53f785f73a2f6d00d3c779c14e271ac36c618e
jacobrees <jacob.rees97@yahoo.com>
2020-08-28 11:34:29 +0100

    Made create method save title & body & redirect to post

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+    include ArticlesHelper
+    
     def index 
         @articles = Article.all
     end
@@ -11,4 +13,9 @@ class ArticlesController < ApplicationController
         @article = Article.new 
     end
 
+    def create
+        @article = Article.new(article_params)
+        @article.save
+        redirect_to article_path(@article)
+    end
 end
